c69fcde4afc7d55aac1b9e32adf80bdcdc78b194
ice <smalltides@gmail.com>
2017-03-02 01:14:48 +0800

    add sqlite CURD function

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # book_store
-use python3, Tkinter and PostgreSQL to create a book store desktop application
+use python3, Tkinter and sqlite to create a book store desktop application
 
 ![alt text](https://github.com/smalltide/book_store/blob/master/screenshot.gif "book_store")
 
 1. python3
 2. Tkinter
-3. PostgreSQL
+3. sqlite3
 
 ```
   > git@github.com:smalltide/book_store.git
---
+++ backend.py
@@ -0,0 +1,52 @@
+import sqlite3
+
+def connect():
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year INTEGER, isbn INTEGER)")
+    conn.commit()
+    conn.close()
+
+def insert(title, author, year, isbn):
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO book VALUES (NULL, ?, ?, ?, ?)", (title, author, year, isbn))
+    conn.commit()
+    conn.close()
+
+def view():
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book")
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def search(title = "", author = "", year = "", isbn = ""):
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM book WHERE title = ? OR author = ? OR year = ? OR isbn = ?", (title, author, year, isbn))
+    rows = cur.fetchall()
+    conn.close()
+    return rows
+
+def delete(id):
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("DELETE FROM book WHERE id = ?", (id,))
+    conn.commit()
+    conn.close()
+
+def update(id, title, author, year, isbn):
+    conn = sqlite3.connect('books.db')
+    cur = conn.cursor()
+    cur.execute("UPDATE book set title = ?, author = ?, year = ?, isbn = ? WHERE id = ?", (title, author, year, isbn, id))
+    conn.commit()
+    conn.close()
+
+connect()
+#insert("The sea", "Jhon", 1918, 123456)
+#delete(3)
+update(4, "The sea2", "ice", 1920, 23456)
+print(view())
+print(search(author = "ice"))
